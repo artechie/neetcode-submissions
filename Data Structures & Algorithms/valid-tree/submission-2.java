c1a98dd7d30324c7ad2bda8d6eacccf5d679c608
Archit Tewari <archit.mart@gmail.com>
2026-07-08 20:18:48 +0530

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        List<Integer>[] g = new ArrayList[n];
+
+        for (int i = 0; i < n; i++) {
+            g[i] = new ArrayList<>();
+        }
+
+        for (int[] e : edges) {
+            g[e[0]].add(e[1]);
+            g[e[1]].add(e[0]);
+        }
+
+        boolean[] vis = new boolean[n];
+
+        Deque<int[]> dq = new ArrayDeque<>();
+        dq.add(new int[] {0, -1});
+        
+        vis[0] = true;
+
+        while (!dq.isEmpty()) {
+            int[] rem = dq.poll();
+
+            for (int nbr : g[rem[0]]) {
+                if (nbr == rem[1]) {
+                    continue;
+                }
+                
+                if (vis[nbr]) {
+                    return false;
+                }
+                
+                dq.add(new int[] {nbr, rem[0]});
+                vis[nbr] = true;
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+            if (!vis[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
