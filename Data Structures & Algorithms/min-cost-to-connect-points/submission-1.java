fd78b523519df28b54c0dfe557e1e1aac6c390a7
Archit Tewari <archit.mart@gmail.com>
2026-07-11 22:07:19 +0530

    Add: min-cost-to-connect-points - submission-1

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-1.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int minCostConnectPoints(int[][] points) {
+        int n = points.length;
+        Set<Integer> vis = new HashSet<>();
+        int[] d = new int[n];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((p, q) -> Integer.compare(p[1], q[1]));
+        pq.add(new int[] {0, 0});
+
+        Arrays.fill(d, Integer.MAX_VALUE);
+
+        d[0] = 0;
+
+        while (vis.size() < n) {
+            int[] rem = pq.poll();
+
+            if (vis.contains(rem[0])) {
+                continue;
+            }
+
+            vis.add(rem[0]);
+
+            for (int i = 0; i < n; i++) {
+                if (vis.contains(i)) {
+                    continue;
+                }
+
+                int[] src = points[rem[0]];
+                int[] tar = points[i];
+                int dis = Math.abs(src[0] - tar[0]) + Math.abs(src[1] - tar[1]);
+
+                if (dis < d[i]) {
+                    d[i] = dis;
+                    pq.add(new int[] {i, d[i]});
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(d));
+
+        return Arrays.stream(d).sum();
+    }
+}
